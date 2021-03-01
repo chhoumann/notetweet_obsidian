8a0652a1f3f37f61cbb12d98ab3041888a7c0a05
Christian Bager Bach Houmann <chhoumann@gmail.com>
2021-03-01 17:03:47 +0100

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@ import commonjs from '@rollup/plugin-commonjs';
 export default {
   input: 'main.ts',
   output: {
-    dir: 'D:\\Mapper\\Tools\\Dev\\Projects\\notes\\.obsidian\\plugins\\notetweet-obsidian',
+    dir: '.',
     sourcemap: 'inline',
     format: 'cjs',
     exports: 'default'
